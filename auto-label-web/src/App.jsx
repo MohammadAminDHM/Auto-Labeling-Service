bccf760b37c4bd3d95d6831813937b679396afa9
Alireza Hashemi <alirahshmi@gmail.com>
2025-12-27 15:25:05 +0330

    change the dir name and change all pages files formats to .jsx

--- auto-label-web/src/App.jsx
+++ auto-label-web/src/App.jsx
@@ -1,10 +1,18 @@
 import Layout from "@/Layout";
+import { Routes, Route } from "react-router-dom";
 import Annotate from "@/Pages/Annotate.jsx";
+import Dashboard from "@/Pages/Dashboard.jsx";
+import Landing from "@/Pages/Landing.jsx";
 
 export default function App() {
   return (
-    <Layout currentPageName="Annotate">
-      <Annotate />
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/annotate" element={<Annotate />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Add more routes as needed */}
+      </Routes>
     </Layout>
   );
 }
